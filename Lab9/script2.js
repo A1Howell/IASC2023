6709a7cee901a14cd79754b012e724ca553f6ac0
A1Howell <144639290+A1Howell@users.noreply.github.com>
2023-11-12 19:13:38 -0500

    Update script2.js

--- Lab9/script2.js
+++ Lab9/script2.js
@@ -1,14 +1,14 @@
 // Prompt the user to enter their age
-var userAge = prompt("What is your age?");
+var ageInput = prompt("Please enter your age:");
 
-// Convert the entered age to a Number
-var ageAsNumber = parseInt(userAge);
+// Convert the entered age to a number
+var age = parseInt(ageInput);
 
 // Get the current year
 var currentYear = new Date().getFullYear();
 
 // Calculate the birth year
-var birthYear = currentYear - ageAsNumber;
+var birthYear = currentYear - age;
 
 // Display the birth year on the screen
-document.body.innerHTML = "<h1>Your birth year is: " + birthYear + "</h1>";
+console.log("Your birth year is: " + birthYear);
