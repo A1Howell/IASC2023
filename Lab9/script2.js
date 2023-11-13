c6d85e8023072534d22c46f5b23c2919af14b6a1
A1Howell <144639290+A1Howell@users.noreply.github.com>
2023-11-12 20:14:51 -0500

    Update script2.js

--- Lab9/script2.js
+++ Lab9/script2.js
@@ -11,4 +11,4 @@ var currentYear = new Date().getFullYear();
 var birthYear = currentYear - age;
 
 // Display the birth year on the screen
-console.log("Your birth year is: " + birthYear);
+document.write("Your birth year is: " + birthYear);
