0a36f2d2bfce549a6709ea7fbea9e748a71bc7b7
A1Howell <144639290+A1Howell@users.noreply.github.com>
2023-11-12 19:19:41 -0500

    Update script1.js

--- Lab9/script1.js
+++ Lab9/script1.js
@@ -2,19 +2,16 @@ var startTime;
 
 // Function to initialize the start time when the page loads
 window.onload = function () {
-  startTime = performance.now();
+  startTime = new Date();
 }
 
 // Function to be called when the button is pressed
 function stopTime() {
   // Get the current time when the button is pressed
-  var stopTime = performance.now();
+  var stopTime = new Date();
 
-  // Calculate the time difference in milliseconds
-  var millisecondsPassed = stopTime - startTime;
-
-  // Convert milliseconds to seconds
-  var secondsPassed = Math.floor(millisecondsPassed / 1000);
+  // Calculate the time difference in seconds
+  var secondsPassed = Math.floor((stopTime - startTime) / 600);
 
   // Display the number of seconds in a pop-up
   alert("Seconds passed since the page loaded: " + secondsPassed + " seconds");
